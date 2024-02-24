90a3c9d11089998d25521e7394b8f5fa9dbb8d6e
Itay Fintz <itayfi@gmail.com>
2024-02-24 03:59:21 +0200

    Allow selecting model

--- src/components/ai-generate.tsx
+++ src/components/ai-generate.tsx
@@ -1,6 +1,19 @@
 import { FormEvent, useRef } from "react";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select";
+import { SelectValue } from "@radix-ui/react-select";
+
+const settingsByModel = {
+  "dall-e-2": {
+    model: "dall-e-2",
+    size: "512x512",
+  },
+  "dall-e-3": {
+    model: "dall-e-3",
+    size: "1024x1024",
+  },
+} as const;
 
 type Props = {
   onStart: () => void;
@@ -9,12 +22,13 @@ type Props = {
 };
 export const AiGenerate = ({ onStart, onFinish, onError }: Props) => {
   const promptRef = useRef<HTMLTextAreaElement>(null);
+  const modelRef = useRef<HTMLSelectElement>(null);
   const onGenerate = async (event: FormEvent) => {
     event?.preventDefault();
     onStart();
     const prompt = promptRef.current?.value;
     if (!prompt || prompt === "") {
-      onError()
+      onError();
       return;
     }
 
@@ -31,8 +45,7 @@ export const AiGenerate = ({ onStart, onFinish, onError }: Props) => {
         body: JSON.stringify({
           prompt,
           n: 1,
-          size: "1024x1024",
-          model: "dall-e-3",
+          ...settingsByModel[modelRef.current?.value as keyof typeof settingsByModel],
           response_format: "b64_json",
         }),
       }
@@ -41,18 +54,31 @@ export const AiGenerate = ({ onStart, onFinish, onError }: Props) => {
       onError();
       return;
     }
-    const { data } = (await response.json()) as { data: { b64_json: string }[] };
+    const { data } = (await response.json()) as {
+      data: { b64_json: string }[];
+    };
     const b64_json = data[0].b64_json;
     onFinish(`data:image/png;base64,${b64_json}`);
   };
   return (
-    <form onSubmit={onGenerate} className="text-right space-y-3 mt-4">
+    <form onSubmit={onGenerate} className="space-y-3 mt-4">
       <Textarea
         required
         placeholder="Generate Sticker using AI"
         ref={promptRef}
       />
-      <Button type="submit">Generate</Button>
+      <div className="flex flex-row gap-2 items-centers justify-end">
+        <Select defaultValue="dall-e-3">
+          <SelectTrigger className="inline-flex w-48 h-10">
+            <SelectValue placeholder="Model" ref={modelRef} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="dall-e-2">DALL-e 2</SelectItem>
+            <SelectItem value="dall-e-3">DALL-e 3</SelectItem>
+          </SelectContent>
+        </Select>
+        <Button type="submit">Generate</Button>
+      </div>
     </form>
   );
 };
